d4e1d5575365c51294b97a32d275e7953663a3ae
Dylan <60489717+DRoberts919@users.noreply.github.com>
2021-02-12 18:57:34 -0700

    functional page

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,39 @@
 
 
+
+
+
+
+
+//this is where all of our routes are handled
 exports.index = (req,res) => {
-    res.sendFile('/views/index.html');
+    res.render("index",{
+        title:"index",
+    });
 };
+
+exports.login = (req,res) =>{
+    res.render("login",{
+        title: "index",
+    });
+};
+
+exports.signup = (req,res) =>{
+    res.render("signup",{
+        title:"signup"
+    });
+};
+
+exports.account = (req,res) =>{
+    res.render("account",{
+        title:"account"
+    });
+};
+
+exports.boards = (req,res) =>{
+    res.render("boarsd",{
+        title:"boards"
+    });
+};
+
+
