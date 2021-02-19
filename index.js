2c5b1fc8c1c24ae78d2ea0778c8dc83e02d5d8c3
PhillipDuarte1 <pduarte@student.neumont.edu>
2021-02-19 11:05:33 -0700

    functional page

--- index.js
+++ index.js
@@ -2,22 +2,39 @@
 const http = require('http');
 const express = require('express');
 const routes = require('./routes/routes');
+const path = require('path');
+const bodyParser = require('body-parser');
+const express_session = require('express-session');
 const app = express();
 const port = 3000;
 
-//stattic folders
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/images', express.static(__dirname + 'public/images'));
+
+//pug viewEngine
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')))
 
 
-
-// app.get('/',routes.index);
-app.get('',(req,res) =>{
-    res.sendFile(__dirname + '/views/index.html')
+const urlEncodedParser = bodyParser.urlencoded({
+    extended: true
 });
 
+
+//sessions for when we are loged in to the page
+app.use(express_session({
+    secret: "This is a secret",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+
+//routes that are then handled withing our routes.js file
+app.get('/',routes.index);
+app.get('/login',routes.login);
+app.get('/signup',routes.signup);
+app.get('/account',routes.account);
+app.get('/boards',routes.boards);
 
 
 
