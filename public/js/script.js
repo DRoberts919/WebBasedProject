7528660db4a9712d30b6d1928834d0ce9a0c4b45
Nathan1127 <61479619+Nathan1127@users.noreply.github.com>
2021-02-18 19:34:43 -0700

    Update script.js
    Added the functions for adding to the boards

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,3 @@
-
 const btnClicked = document.getElementById('boardBtn');
 console.log(btnClicked);
 
@@ -18,3 +17,31 @@ CreateBoard = () =>{
     recentsDiv.appendChild(boardDiv);
 }
 
+
+
+function addToDo(){
+    var ul = document.getElementById("to-do-list");
+    var toDoInput = document.getElementById("toDoInput");
+    var li = document.createElement("li");
+    li.setAttribute('id',toDoInput.value);
+    li.appendChild(document.createTextNode(toDoInput.value));
+    ul.appendChild(li);
+}
+
+function addInProgress(){
+    var ul = document.getElementById("in-progress-list");
+    var inProgressInput = document.getElementById("inProgressInput");
+    var li = document.createElement("li");
+    li.setAttribute('id',inProgressInput.value);
+    li.appendChild(document.createTextNode(inProgressInput.value));
+    ul.appendChild(li);
+}
+
+function addFinished(){
+    var ul = document.getElementById("finished-list");
+    var finishedInput = document.getElementById("finishedInput");
+    var li = document.createElement("li");
+    li.setAttribute('id',finishedInput.value);
+    li.appendChild(document.createTextNode(finishedInput.value));
+    ul.appendChild(li);
+}
