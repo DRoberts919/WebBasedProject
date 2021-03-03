28b7933d7cfac128d3b54c06e8e0a4d4e6a321b5
Dylan <60489717+DRoberts919@users.noreply.github.com>
2021-03-02 19:23:50 -0700

    working login
    the login works and displays users name

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 //imports
-const http = require('http');
-const express = require('express');
-const routes = require('./routes/routes');
-const path = require('path');
-const bodyParser = require('body-parser');
-const express_session = require('express-session');
-const User = require('./model/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { response } = require('express');
-require('dotenv').config()
+const http = require("http");
+const express = require("express");
+const routes = require("./routes/routes");
+const path = require("path");
+const bodyParser = require("body-parser");
+const express_session = require("express-session");
+const User = require("./model/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { response } = require("express");
+require("dotenv").config();
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+var cookieParser = require("cookie-parser");
 const port = 3000;
-
-
 
 const JWT_SECRET = process.env.JWT_SECRET;
 console.log(JWT_SECRET);
 //this is for the
 
 //database connection
-mongoose.connect('mongodb://localhost:27017/TrelloUser', {
-    useNewUrlParser: true, //These two are just used to get rid of the warnings/errors
-    useUnifiedTopology: true,
-    useCreateIndex: true
+mongoose.connect("mongodb://localhost:27017/TrelloUser", {
+  useNewUrlParser: true, //These two are just used to get rid of the warnings/errors
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 //pug viewEngine
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use('/', express.static(path.join(__dirname + '/public')))
-app.use(bodyParser.json()) //decode the body thats coming in(JSON)
-
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/", express.static(path.join(__dirname + "/public")));
+app.use(bodyParser.json()); //decode the body thats coming in(JSON)
+app.use(cookieParser());
 
 const urlEncodedParser = bodyParser.urlencoded({
-    extended: true
+  extended: true,
 });
 
-
 //sessions for when we are loged in to the page
-app.use(express_session({
+app.use(
+  express_session({
     secret: "This is a secret",
     saveUninitialized: true,
     resave: true,
@@ -48,73 +47,79 @@ app.use(express_session({
 );
 
 //routes that are then handled withing our routes.js file
-app.get('/',routes.index);
-app.get('/login',routes.login);
-app.get('/signup',routes.signup);
-app.get('/account',urlEncodedParser,routes.account);
-app.post('/account',urlEncodedParser,routes.checkUser);
-app.get('/boards',routes.boards);
-//Don't have to do it this way 
+app.get("/", routes.index);
+app.get("/login", routes.login);
+app.get("/signup", routes.signup);
+
+app.get("/account", (req, res) => {
+  console.log("Cookies: ", req.cookies);
+  const token = req.cookies.Authorization.split(" ")[1];
+  console.log(token);
+
+  const decodedToken = jwt.decode(token);
+  console.log(decodedToken);
+
+  
+
+  const userInfo = {
+      'username': decodedToken.username,
+      'id': decodedToken.id
+  }
+
+  res.render("account",{userInfo});
+});
+
+app.post("/account", routes.checkUser);
+app.get("/boards", routes.boards);
+//Don't have to do it this way
 // app.post('/boards/:boardId',routes.createboard);
 
-
-
-
-
-
-app.post('/api/login', async (req, res) => {
+app.post("/api/login", async (req, res) => {
   console.log("/api/login is working");
-  const { username, password } = req.body
-    console.log(username)
-    console.log(password)
-  const userLogin = await User.findOne({ username: username }).lean() //lean returns a simple json rep of the document
+  const { username, password } = req.body;
+  console.log(username);
+  console.log(password);
+  const userLogin = await User.findOne({ username: username }).lean(); //lean returns a simple json rep of the document
 
-  if(userLogin == null) {
-      return res.json({ status: 'error', error: 'Invalid username/password'})
+  if (userLogin == null) {
+    res.json({ status: "error", error: "Invalid username/password" });
+  } else {
+    if (await bcrypt.compare(password, userLogin.password)) {
+      //its successful it finds the record, then the passwords compare to eachothers even with hash
+
+      const token = jwt.sign(
+        {
+          id: userLogin._id,
+          username: userLogin.username,
+          board: userLogin.board
+        },
+        JWT_SECRET
+      );
+      res.cookie("Authorization", "Bearer " + token);
+
+      // res.json({ status: 'ok', data: token})
+      //   res.json({ status: "ok", data: token });
+
+      //   req.session.user = userLogin;
+
+      res.redirect(302, "/account");
+    } else {
+      res.json({ status: "error", error: "Invalid username/password" });
+    }
   }
-  else{
+});
 
-    if(await bcrypt.compare(password, userLogin.password)) {
-        //its successful it finds the record, then the passwords compare to eachothers even with hash
-  
-        const token = jwt.sign
-            ({ 
-                id: userLogin._id, 
-                username: userLogin.username
-            }, 
-            JWT_SECRET
-        );
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
 
-        
-        // res.json({ status: 'ok', data: token})
-        res.json({ status: 'ok',data: token });
-        
+  const { username, password: plainTextPassword } = req.body;
 
-        req.session.user = userLogin,
-            res.redirect('/account');
-    }
-
-    
-
-    res.json({ status: 'error', error: 'Invalid username/password' })
-    
-    }
-
-})
-
-
-
-app.post('/api/register', async (req, res) => {
-  console.log(req.body)
-
-  const { username, password: plainTextPassword } = req.body
-
-  if(!username || typeof username !== 'string') {
-      return res.json({ status: 'error', error: 'Invalid username'})
+  if (!username || typeof username !== "string") {
+    return res.json({ status: "error", error: "Invalid username" });
   }
 
-  if(!plainTextPassword || typeof plainTextPassword !== 'string') {
-      return res.json({ status: 'error', error: 'Invalid password'})
+  if (!plainTextPassword || typeof plainTextPassword !== "string") {
+    return res.json({ status: "error", error: "Invalid password" });
   }
 
   /*
@@ -123,33 +128,24 @@ app.post('/api/register', async (req, res) => {
   }
   */
 
-  const password = await bcrypt.hash(plainTextPassword, 10)
+  const password = await bcrypt.hash(plainTextPassword, 10);
 
   try {
-      const response = await User.create({
-          username,
-          password
-      })
-      console.log('User created successfully', response)
-  } catch(error) {
-      if(error.code === 11000) {
-          return res.json({ status: 'error', error: 'Username already in use'})
-      }
-      throw error
+    const response = await User.create({
+      username,
+      password,
+    });
+    console.log("User created successfully", response);
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.json({ status: "error", error: "Username already in use" });
+    }
+    throw error;
   }
 
-  res.json({status: 'ok'})
-})
+  res.json({ status: "ok" });
+});
 //test commit
 
-
 //app listening on port 3000
-app.listen(port,() => console.info(`Listening on port ${port}`));
-
-
-
-
-
-
-
-
+app.listen(port, () => console.info(`Listening on port ${port}`));
--- public/js/script.js
+++ public/js/script.js
@@ -1,225 +1,222 @@
-const btnClicked = document.getElementById('boardBtn');
+const btnClicked = document.getElementById("boardBtn");
 console.log(btnClicked);
 
 // btnClicked.onclick(CreateBoard());
 
+CreateBoard = () => {
+  // here we create the board or just a fancy div
+  const boardDiv = document.createElement("div");
+  console.log(boardDiv);
 
-CreateBoard = () =>{
-    // here we create the board or just a fancy div
-    const boardDiv = document.createElement('div');
-    console.log(boardDiv);
+  //add a class to the board for styling
+  boardDiv.classList.add("board");
+  boardDiv.id = "usersBoard";
 
-    //add a class to the board for styling
-    boardDiv.classList.add('board');
-    boardDiv.id = 'usersBoard';
-    
+  var recentsDiv = document.getElementById("currentBoard");
 
-    var recentsDiv = document.getElementById('currentBoard');
-
-    recentsDiv.appendChild(boardDiv);
-}
-
-
+  recentsDiv.appendChild(boardDiv);
+};
 
 function addToDo() {
-    var a = document.getElementById("to-do-list");
-    var b = document.createElement("input");
+  var a = document.getElementById("to-do-list");
+  var b = document.createElement("input");
 
-    b.setAttribute("type", "text");
-    b.classList.add('listItem');
-    b.draggable = "true";
+  b.setAttribute("type", "text");
+  b.classList.add("listItem");
+  b.draggable = "true";
 
-    a.appendChild(b);
+  a.appendChild(b);
 
-    b.addEventListener('dragstart', function () {
-        console.log('dragstart');
-        draggedItem = b;
-        setTimeout(function () {
-            b.style.display = 'none';
-        }, 0);
-    });
-    
-    b.addEventListener('dragend', function () {
-        console.log('dragend');
-        setTimeout(function () {
-            draggedItem = b;
-            draggedItem.style.display = 'block';
-            draggedItem = null;
-        }, 0);
-    });
+  b.addEventListener("dragstart", function () {
+    console.log("dragstart");
+    draggedItem = b;
+    setTimeout(function () {
+      b.style.display = "none";
+    }, 0);
+  });
+
+  b.addEventListener("dragend", function () {
+    console.log("dragend");
+    setTimeout(function () {
+      draggedItem = b;
+      draggedItem.style.display = "block";
+      draggedItem = null;
+    }, 0);
+  });
 }
 
 function addInProgress() {
-    var a = document.getElementById("in-progress-list");
-    var b = document.createElement("input");
+  var a = document.getElementById("in-progress-list");
+  var b = document.createElement("input");
 
-    b.setAttribute("type", "text");
-    b.classList.add('listItem');
-    b.draggable = "true";
+  b.setAttribute("type", "text");
+  b.classList.add("listItem");
+  b.draggable = "true";
 
-    a.appendChild(b);
+  a.appendChild(b);
 
-    b.addEventListener('dragstart', function () {
-        console.log('dragstart');
-        draggedItem = b;
-        setTimeout(function () {
-            b.style.display = 'none';
-        }, 0);
-    });
-    
-    b.addEventListener('dragend', function () {
-        console.log('dragend');
-        setTimeout(function () {
-            draggedItem = b;
-            draggedItem.style.display = 'block';
-            draggedItem = null;
-        }, 0);
-    });
+  b.addEventListener("dragstart", function () {
+    console.log("dragstart");
+    draggedItem = b;
+    setTimeout(function () {
+      b.style.display = "none";
+    }, 0);
+  });
+
+  b.addEventListener("dragend", function () {
+    console.log("dragend");
+    setTimeout(function () {
+      draggedItem = b;
+      draggedItem.style.display = "block";
+      draggedItem = null;
+    }, 0);
+  });
 }
 
 function addFinished() {
-    var a = document.getElementById("finished-list");
-    var b = document.createElement("input");
+  var a = document.getElementById("finished-list");
+  var b = document.createElement("input");
 
-    b.setAttribute("type", "text");
-    b.classList.add('listItem');
-    b.draggable = "true";
+  b.setAttribute("type", "text");
+  b.classList.add("listItem");
+  b.draggable = "true";
 
-    a.appendChild(b);
+  a.appendChild(b);
 
-    b.addEventListener('dragstart', function () {
-        console.log('dragstart');
-        draggedItem = b;
-        setTimeout(function () {
-            b.style.display = 'none';
-        }, 0);
-    });
-    
-    b.addEventListener('dragend', function () {
-        console.log('dragend');
-        setTimeout(function () {
-            draggedItem = b;
-            draggedItem.style.display = 'block';
-            draggedItem = null;
-        }, 0);
-    });
+  b.addEventListener("dragstart", function () {
+    console.log("dragstart");
+    draggedItem = b;
+    setTimeout(function () {
+      b.style.display = "none";
+    }, 0);
+  });
+
+  b.addEventListener("dragend", function () {
+    console.log("dragend");
+    setTimeout(function () {
+      draggedItem = b;
+      draggedItem.style.display = "block";
+      draggedItem = null;
+    }, 0);
+  });
 }
 
-
-
-const list_items = document.querySelectorAll('.listItem');
-const lists = document.querySelectorAll('.list');
+const list_items = document.querySelectorAll(".listItem");
+const lists = document.querySelectorAll(".list");
 
 for (let i = 0; i < list_items.length; i++) {
-    const item = list_items[i];
-    
-    item.addEventListener('dragstart', function () {
-        console.log('dragstart');
-        draggedItem = item;
-        setTimeout(function () {
-            item.style.display = 'none';
-        }, 0);
-    });
-    
-    item.addEventListener('dragend', function () {
-        console.log('dragend');
-        setTimeout(function () {
-            draggedItem.style.display = 'block';
-            draggedItem = null;
-        }, 0);
-    });
+  const item = list_items[i];
+
+  item.addEventListener("dragstart", function () {
+    console.log("dragstart");
+    draggedItem = item;
+    setTimeout(function () {
+      item.style.display = "none";
+    }, 0);
+  });
+
+  item.addEventListener("dragend", function () {
+    console.log("dragend");
+    setTimeout(function () {
+      draggedItem.style.display = "block";
+      draggedItem = null;
+    }, 0);
+  });
 }
 for (let j = 0; j < lists.length; j++) {
-    const list = lists[j];
-    
-    list.addEventListener('dragover' ,function (e) {
-        e.preventDefault();
-        this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
-    });
-    
-    list.addEventListener('dragenter', function(e) {
-        e.preventDefault();
-        this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
-    });
-    
-    list.addEventListener('dragleave', function(e) {
-        this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-    });
-    
-    list.addEventListener('drop', function (e) {
-        console.log('drop');
-        this.append(draggedItem);
-        this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-    });
+  const list = lists[j];
+
+  list.addEventListener("dragover", function (e) {
+    e.preventDefault();
+    this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+  });
+
+  list.addEventListener("dragenter", function (e) {
+    e.preventDefault();
+    this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+  });
+
+  list.addEventListener("dragleave", function (e) {
+    this.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  });
+
+  list.addEventListener("drop", function (e) {
+    console.log("drop");
+    this.append(draggedItem);
+    this.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  });
 }
 
 //Phill's Static JS
 
-var regform = document.getElementById('reg-form');
+var regform = document.getElementById("reg-form");
 if (regform) {
-    regform.addEventListener('submit', registerUser)
+  regform.addEventListener("submit", registerUser);
 }
 //fetch is a utility used by browsers to make HTTP calls
 async function registerUser(event) {
-    event.preventDefault()
-    const username = document.getElementById('username').value; //Want the value and not the DOM because stores as empty objects
-    const password = document.getElementById('password').value;
+  event.preventDefault();
+  const username = document.getElementById("username").value; //Want the value and not the DOM because stores as empty objects
+  const password = document.getElementById("password").value;
 
-    //Grabs from the input fields and pass it along the stringify function
-    const result = await fetch('/api/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username,
-            password
-        })
-    }).then((res) => res.json())
+  //Grabs from the input fields and pass it along the stringify function
+  const result = await fetch("/api/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  }).then((res) => res.json());
 
-    if(result.status === 'ok') {
-        //alert('Success')
-        
-    } else {
-        alert(result.error)
-    }
+  if (result.status === "ok") {
+    //alert('Success')
+  } else {
+    alert(result.error);
+  }
 
-    console.log(result);
+  console.log(result);
 }
 
-var loginform = document.getElementById('login');
+var loginform = document.getElementById("login");
 if (loginform) {
-    loginform.addEventListener('submit', login);
+  loginform.addEventListener("submit", login);
 }
 
 async function login(event) {
-    event.preventDefault()
-    const username = document.getElementById('username').value; //Want the value and not the DOM because stores as empty objects
-    const password = document.getElementById('password').value;
+  event.preventDefault();
+  const username = document.getElementById("username").value; //Want the value and not the DOM because stores as empty objects
+  const password = document.getElementById("password").value;
 
-    //Grabs from the input fields and pass it along the stringify function
-    const result = await fetch('/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username,
-            password
-        })
-    }).then((res) => res.json())
+  //Grabs from the input fields and pass it along the stringify function
+  const result = await fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+      
+    }),
+  });
+  
 
-    if(result.status === 'ok') {
-        console.log('Got the token:', result.data)
-        localStorage.setItem('token', result.data) //can persist on refreshes as well
-        //alert('Success')
-        window.location.href = "/account"
-        
-    } else {
-        alert(result.error)
-    }
+  if(result.status === 200) {
+      
+      localStorage.setItem('userName', username) //can persist on refreshes as well
+      //alert('Success')
+      window.location.href = "/account"
 
-    console.log(result);
+  } else {
+    localStorage.setItem('userName', null) //can persist on refreshes as well
+    
+    // alert(result.error)
+  }
+
+  console.log(result);
 }
 
 // var changeform = document.getElementById('change-form');
