85ab695d985c4f3cd4bd8553518a56d5014de655
RUnroe <ryanunroe@gmail.com>
2021-10-15 14:50:14 -0600

    Initialize board and user routes

--- back-end/index.js
+++ back-end/index.js
@@ -10,7 +10,7 @@ const dal = require('./db/dal');
 
 
 // app.set("trust proxy", 1);
-app.use(express.static(path.join(__dirname + "/public")));
+// app.use(express.static(path.join(__dirname + "/public")));
 
 app.use(bodyParser.urlencoded({
         extended: true
@@ -20,23 +20,23 @@ app.use(bodyParser.json());
 
 //app.use(require('cookie-parser')(require('./secrets').session.secret));
 
-app.use(session({
-        store: MongoStore.create({
-                mongoUrl: require('./secrets').mongo.connectionString,
-                dbName: 'Bello'
-        }),
-        secret: require('./secrets').session.secret,
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 30,
-                sameSite: 'lax',
-                secure: 'auto',
-                httpOnly: false
-        }
-}));
+// app.use(session({
+//         store: MongoStore.create({
+//                 mongoUrl: require('./secrets').mongo.connectionString,
+//                 dbName: 'Bello'
+//         }),
+//         secret: require('./secrets').session.secret,
+//         resave: false,
+//         saveUninitialized: false,
+//         cookie: {
+//                 maxAge: 1000 * 60 * 60 * 24 * 30,
+//                 sameSite: 'lax',
+//                 secure: 'auto',
+//                 httpOnly: false
+//         }
+// }));
 
-let routeFiles = ['api/orders', 'api/users', 'api/products', 'api/icons'];
+let routeFiles = ['api/boards', 'api/users'];
 const routeManager = require('./routes/manager');
 routeFiles.forEach((file) => {
         let component = require(`./routes/${file}`);
---
+++ back-end/routes/api/boards.js
@@ -0,0 +1,86 @@
+const dal = {};
+const configure = (obj) => {
+	Object.assign(dal, obj.dal);
+
+};
+
+const { requireAuth, requireNotAuth, handle } = require('../util');
+
+const createBoard = (req, res) => {
+	dal.createBoard(req.session.user_id, req.body).then((result) => {
+		res.json(result);
+	})
+	.catch(handle(req, res));
+}
+
+const getBoard = (req, res) => {
+	dal.getBoardById(req.params.board_id).then(result => {
+		res.json(result);
+	})
+	.catch(handle(req, res));
+}
+
+
+const getBoardsByUser = (req, res) => {
+	dal.getBoardsByUser(req.params.user_id).then(result => {
+		res.json(result);
+	})
+	.catch(handle(req, res));
+}
+
+
+
+
+//Variable data pieces: title, price, description, category, cover_image, page_structure
+const updateBoard = (req, res) => {
+	dal.updateBoard(req.body.board_id, req.session.user_id, req.body).then(() => {
+		res.status(201);
+		res.statusMessage = 'Updated Board';
+		res.end();
+	})
+	.catch(handle(req, res));
+}
+
+
+const removeBoard = (req, res) => {
+	dal.removeBoard(req.body.board_id).then(() => {
+		res.status(204);
+		res.statusMessage = 'Removed Board';
+		res.end();
+	})
+	.catch(handle(req, res));
+}
+
+
+
+
+const routes = [
+	{
+		uri: '/api/board',
+		methods: ['post'],
+		handler: [requireAuth(), createBoard]
+	},
+    {
+		uri: '/api/board/:board_id',
+		methods: ['get'],
+		handler: [requireAuth(), getBoard]
+	},
+	{
+		uri: '/api/board',
+		methods: ['get'],
+		handler: [requireAuth(), getBoardsByUser]
+	},
+    {
+		uri: '/api/board/:board_id',
+		methods: ['put'],
+		handler: [requireAuth(), updateBoard]
+	},
+    {
+		uri: '/api/board/:board_id',
+		methods: ['delete'],
+		handler: [requireAuth(), removeBoard]
+	}
+];
+
+
+module.exports = { routes, configure };
