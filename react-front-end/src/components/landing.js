6806d8e1acd7cbdd120538ebc0925c44acd0203c
Joshua <jgorham@student.neumont.edu>
2021-10-21 14:11:45 -0600

    Landing with Session existing

--- react-front-end/src/components/landing.js
+++ react-front-end/src/components/landing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-export default function Landing() {
+function LandingWithOutSession() {
     return(
         <>
         <div className="section">
@@ -26,3 +26,29 @@ export default function Landing() {
         </>
     )
 }
+
+function LandingWithSession(){
+    return(
+        <>
+        <div className="session">
+            <div className="container">
+                <h1 className="text-center">Your Boards</h1>
+            </div>
+        </div>
+        <div className="board-list">
+            <Link to="/board/:board_id" className="board-card">
+                <div className="color-section"></div>
+                <div className="text-section"><p>Board Name</p></div>
+            </Link>
+        </div>
+        <div className="board-list">
+            <Link to="/board/:board_id" className="board-card">
+                <div className="color-section"></div>
+                <div className="text-section"><p>Board Name</p></div>
+            </Link>
+        </div>
+        </>
+    )
+}
+
+export default LandingWithSession;
