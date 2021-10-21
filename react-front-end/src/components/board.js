e9edc698e5778f020b898e5c65ce3d49c007050b
Joshua <jgorham@student.neumont.edu>
2021-10-21 14:32:45 -0600

    Board task list cards added

--- react-front-end/src/components/board.js
+++ react-front-end/src/components/board.js
@@ -2,8 +2,35 @@ import React from "react";
 
 export default function Board() {
     return(
+        <>
         <div className="section">
-            <h2 className="text-center">Project Name</h2>
+            <div className="container">
+                <h2 className="text-center">Project Name</h2>
+            </div>
         </div>
+        <div className="list-container">
+            <div className="task-list">
+                <div className="list-header">List 1</div>
+                <div className="list-body">
+                    <div className="task-card">
+                        <p>Card 1</p>
+                        <div className="task-btn"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="task-list">
+                <div className="list-header">List 2</div>
+                <div className="list-body">
+                    <div className="task-card">
+                        <p>Card 1</p>
+                        <div className="task-btn"></div>
+                    </div><div className="task-card">
+                        <p>Card 2</p>
+                        <div className="task-btn"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
